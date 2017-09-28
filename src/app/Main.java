376f43f68a1fed39837acaec126e269f2a34cf63
yao <yaoshi616@gmail.com>
2017-09-27 22:05:05 -0400

    Fix UI. Add App launcher

---
+++ src/app/Main.java
@@ -0,0 +1,27 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		// TODO Auto-generated method stub
+
+		Parent root = FXMLLoader.load(getClass().getResource("/view/LibraryUI.fxml"));
+
+		Scene scene = new Scene(root);
+
+		stage.setTitle("FXML Welcome");
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
